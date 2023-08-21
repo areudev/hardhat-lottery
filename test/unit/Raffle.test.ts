95ac59c2db506938606bb1736b53dd4d2b989667
Christos Kalivas <chrkalivas@gmail.com>
2023-08-21 22:14:27 +0300

    unit tests wip

--- test/unit/Raffle.test.ts
+++ test/unit/Raffle.test.ts
@@ -15,21 +15,25 @@ if (!developmentChains.includes(network.name)) {
   describe('Raffle', () => {
     let raffle: Raffle
     let vrfCoordinatorV2Mock: VRFCoordinatorV2Mock
+    let raffleContract: Raffle
     let deployer: SignerWithAddress
     let player: SignerWithAddress
-    let entranceFee: BigNumberish
-    let interval: BigNumberish
+    let entranceFee: bigint
+    let interval: bigint
+    let accounts: SignerWithAddress[]
 
     beforeEach(async () => {
       // const {deployer} = await ethers.getNamedSigners()
-      const accounts = await ethers.getSigners()
+      accounts = await ethers.getSigners()
       ;[deployer, player] = accounts
       await deployments.fixture(['mocks', 'raffle'])
       vrfCoordinatorV2Mock = await ethers.getContract(
         'VRFCoordinatorV2Mock',
         deployer,
       )
-      raffle = await ethers.getContract('Raffle', deployer)
+      raffleContract = await ethers.getContract('Raffle')
+      // raffle = await ethers.getContract('Raffle', deployer)
+      raffle = raffleContract.connect(player)
       entranceFee = await raffle.getEntranceFee()
       interval = await raffle.getInterval()
     })
@@ -62,7 +66,7 @@ if (!developmentChains.includes(network.name)) {
           value: entranceFee,
         })
         const playerFromContract = await raffle.getPlayer(0)
-        assert.equal(playerFromContract, deployer.address)
+        assert.equal(playerFromContract, player.address)
       })
       it('emmits event on enter', async () => {
         await expect(raffle.enterRaffle({value: entranceFee})).to.emit(
@@ -133,141 +137,91 @@ if (!developmentChains.includes(network.name)) {
         // console.log('logs', logs)
         // console.log('requestId', requestId)
         const raffleState = await raffle.getRaffleState()
-
         assert.equal(raffleState.toString(), '1')
         assert(ethers.toNumber(requestId) > 0)
-
         // console.log('txReceipt', txReceipt?.logs[0])
         // console.log('raffleState', raffleState.toString())
       })
     })
+    describe('fulfillRandomWords', () => {
+      beforeEach(async () => {
+        await raffle.enterRaffle({value: entranceFee})
+        await time.increase(ethers.toNumber(interval) + 1)
+      })
+      it('can only be called after performupkeep', async () => {
+        await expect(
+          vrfCoordinatorV2Mock.fulfillRandomWords(0, raffle.target),
+        ).to.be.revertedWith('nonexistent request')
+        await expect(
+          vrfCoordinatorV2Mock.fulfillRandomWords(1, raffle.target),
+        ).to.be.revertedWith('nonexistent request')
+      })
+      it('picks a winner, resets, and sends money', async () => {
+        const additionalEntrances = 3
+        const startingIndex = 2
+        for (
+          let i = startingIndex;
+          i < startingIndex + additionalEntrances;
+          i++
+        ) {
+          // console.log('account', i, accounts[i].address)
+          raffle = raffle.connect(accounts[i])
+          await raffle.enterRaffle({value: entranceFee})
+          // raffle = raffleContract.connect(accounts[i])
+        }
+
+        const startingTimestamp = await raffle.getLastTimeStamp()
+
+        await new Promise<void>(async (resolve, reject) => {
+          ;(raffle as any).once('WinnerPicked', async () => {
+            console.log('WinnerPicked')
+
+            try {
+              const recentWinner = await raffle.getRecentWinner()
+              const raffleState = await raffle.getRaffleState()
+              const lastTimeStamp = await raffle.getLastTimeStamp()
+              const numPlayers = await raffle.getNumberOfPlayers()
+              const winnerBalanceEnd = await ethers.provider.getBalance(
+                accounts[startingIndex].address,
+              )
+
+              assert.equal(numPlayers.toString(), '0')
+              assert.equal(raffleState.toString(), '0')
+              assert(lastTimeStamp > startingTimestamp)
+              assert.equal(recentWinner, accounts[startingIndex].address)
+              assert(
+                winnerBalanceEnd.toString(),
+                (
+                  winnerBalanceStart +
+                  entranceFee * BigInt(additionalEntrances.toString())
+                ).toString(),
+              )
+            } catch (e) {
+              reject(e)
+            }
+            resolve()
+          })
+          const tx = await raffle.performUpkeep('0x')
+          const txReceipt = await tx.wait(1)
+          const winnerBalanceStart = await ethers.provider.getBalance(
+            accounts[startingIndex].address,
+          )
+          console.log('starting balance', winnerBalanceStart.toString())
+          if (!txReceipt) {
+            throw new Error('No transaction receipt')
+          }
+          const logs = await raffle.queryFilter(
+            raffle.filters.RequestedRaffleWinner(),
+            txReceipt.blockHash,
+          )
+          const requestId = logs[0].args?.requestId
+
+          await vrfCoordinatorV2Mock.fulfillRandomWords(
+            requestId,
+            raffle.target,
+          )
+        })
+      })
+    })
   })
 }
-
-// import {
-//   time,
-//   loadFixture,
-// } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-// import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-// import { expect } from "chai";
-// import { ethers } from "hardhat";
-
-// describe("Lock", function () {
-//   // We define a fixture to reuse the same setup in every test.
-//   // We use loadFixture to run this setup once, snapshot that state,
-//   // and reset Hardhat Network to that snapshot in every test.
-//   async function deployOneYearLockFixture() {
-//     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-//     const ONE_GWEI = 1_000_000_000;
-
-//     const lockedAmount = ONE_GWEI;
-//     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-//     // Contracts are deployed using the first signer/account by default
-//     const [owner, otherAccount] = await ethers.getSigners();
-
-//     const Lock = await ethers.getContractFactory("Lock");
-//     const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-//     return { lock, unlockTime, lockedAmount, owner, otherAccount };
-//   }
-
-//   describe("Deployment", function () {
-//     it("Should set the right unlockTime", async function () {
-//       const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-//       expect(await lock.unlockTime()).to.equal(unlockTime);
-//     });
-
-//     it("Should set the right owner", async function () {
-//       const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-//       expect(await lock.owner()).to.equal(owner.address);
-//     });
-
-//     it("Should receive and store the funds to lock", async function () {
-//       const { lock, lockedAmount } = await loadFixture(
-//         deployOneYearLockFixture
-//       );
-
-//       expect(await ethers.provider.getBalance(lock.target)).to.equal(
-//         lockedAmount
-//       );
-//     });
-
-//     it("Should fail if the unlockTime is not in the future", async function () {
-//       // We don't use the fixture here because we want a different deployment
-//       const latestTime = await time.latest();
-//       const Lock = await ethers.getContractFactory("Lock");
-//       await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-//         "Unlock time should be in the future"
-//       );
-//     });
-//   });
-
-//   describe("Withdrawals", function () {
-//     describe("Validations", function () {
-//       it("Should revert with the right error if called too soon", async function () {
-//         const { lock } = await loadFixture(deployOneYearLockFixture);
-
-//         await expect(lock.withdraw()).to.be.revertedWith(
-//           "You can't withdraw yet"
-//         );
-//       });
-
-//       it("Should revert with the right error if called from another account", async function () {
-//         const { lock, unlockTime, otherAccount } = await loadFixture(
-//           deployOneYearLockFixture
-//         );
-
-//         // We can increase the time in Hardhat Network
-//         await time.increaseTo(unlockTime);
-
-//         // We use lock.connect() to send a transaction from another account
-//         await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-//           "You aren't the owner"
-//         );
-//       });
-
-//       it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-//         const { lock, unlockTime } = await loadFixture(
-//           deployOneYearLockFixture
-//         );
-
-//         // Transactions are sent using the first signer by default
-//         await time.increaseTo(unlockTime);
-
-//         await expect(lock.withdraw()).not.to.be.reverted;
-//       });
-//     });
-
-//     describe("Events", function () {
-//       it("Should emit an event on withdrawals", async function () {
-//         const { lock, unlockTime, lockedAmount } = await loadFixture(
-//           deployOneYearLockFixture
-//         );
-
-//         await time.increaseTo(unlockTime);
-
-//         await expect(lock.withdraw())
-//           .to.emit(lock, "Withdrawal")
-//           .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-//       });
-//     });
-
-//     describe("Transfers", function () {
-//       it("Should transfer the funds to the owner", async function () {
-//         const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-//           deployOneYearLockFixture
-//         );
-
-//         await time.increaseTo(unlockTime);
-
-//         await expect(lock.withdraw()).to.changeEtherBalances(
-//           [owner, lock],
-//           [lockedAmount, -lockedAmount]
-//         );
-//       });
-//     });
-//   });
-// });
